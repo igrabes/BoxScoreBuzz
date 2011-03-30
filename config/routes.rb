388413e896e7eb1bb2a7a077a2eea571b0666d38
Ian <ian.grabill@gmail.com>
2011-03-29 22:23:25 -0400

    Testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BoxScoreBuzz::Application.routes.draw do
+  
+
+  resources :posts
+
+  root :to => "home#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ BoxScoreBuzz::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  # 
 
   # See how all your routes lay out with "rake routes"
 
